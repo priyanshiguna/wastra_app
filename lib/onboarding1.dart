b4b853e153ad96c25033a5b64417c152a34ef12d
priyanshi guna <gunapriyanshi37@gmail.com>
2023-03-02 12:04:05 +0530

    priyanshi(font and assets new)

--- lib/onboarding1.dart
+++ lib/onboarding1.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-class Onboarding1 extends StatefulWidget {
-  const Onboarding1({Key? key}) : super(key: key);
+class OnBoarding1 extends StatefulWidget {
+  const OnBoarding1({Key? key}) : super(key: key);
 
   @override
-  State<Onboarding1> createState() => _Onboarding1State();
+  State<OnBoarding1> createState() => _OnBoarding1State();
 }
 
-class _Onboarding1State extends State<Onboarding1> {
+class _OnBoarding1State extends State<OnBoarding1> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
